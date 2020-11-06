820865d9c2b2ef5b4106a1f40c7badda684f601c
Griffin Sullivan <gxs331@psu.edu>
2020-11-06 18:28:59 -0500

    getting server started

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+
+app.use(cors());
+
+const mongoUri = "mongodb+srv://" + process.env.MONGO_USR + ":" + process.env.MONGO_PASS + "@" + process.env.MONGO_URL + "/" + process.env.MONGO_DB + "?retryWrites=true&w=majority";
+const APP_PORT = process.env.PORT || 5000;
+
+mongoose.connect(
+    mongoUri,
+    { useNewUrlParser: true, useUnifiedTopology: true}
+);
+
+app.use(express.json());
+app.use(express.static('client/dist'));
+
+
+app.listen(APP_PORT, () => {console.log(`Trim.io API Started at port: ${APP_PORT}`)});
