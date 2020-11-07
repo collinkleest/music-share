c57ff5b543dd11e6f68798edcdf83b9f98e7c7b3
Collin Kleest <collinkleest@gmail.com>
2020-11-06 19:41:41 -0500

    basic react-router routing

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import {HashRouter, Route} from "react-router-dom";
+import {Home} from './Home';
+import {Login} from './Login';
+import {Signup} from './Signup';
 
 export class App extends React.Component{
   render() {
     return (
-      <div>
-          Hello World
-      </div>
+      <HashRouter>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+      </HashRouter>
     )
   }
 }
