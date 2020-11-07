9dcca058fd986a4c6ce7e4519278c520f1603593
Griffin Sullivan <gxs331@psu.edu>
2020-11-07 17:34:41 -0500

    fixing css in music login

--- client/src/Components/Music Login/MusicLogin.js
+++ client/src/Components/Music Login/MusicLogin.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import './index.css';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faspotify } from '@fortawesome/free-brands-svg-icons';
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  align-items: center;
+  text-align: center;
+`;
+
+const StyledA = styled.a`
+    color: white;
+    text-decoration: none;
+`;
 
 const MusicLogin = (props) => {
     return (
-        <div>
+        <StyledDiv>
             <h1>
                 Select a streaming provider to login with
             </h1>
-            <a href='/login'>
+            <StyledA href='/login'>
                 <Button variant="contained" color="primary">
                     Login with Spotify
                 </Button>
-            </a>
-        </div> 
+            </StyledA>
+        </StyledDiv> 
     )
 }
 
