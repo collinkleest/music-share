125fee5248a88b6e78a3c37099cd778a831a1363
Collin Kleest <collinkleest@gmail.com>
2020-11-08 20:09:58 -0500

    spotify token saved to state

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -6,29 +6,42 @@ import {SignUp} from './Signup';
 import {Password} from "./Password";
 import {MusicLogin} from "./Music Login";
 import { withCookies } from 'react-cookie';
+import { SpotifyApiContext } from 'react-spotify-api';
+import {connect} from "react-redux";
+import compose from "recompose/compose";
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        loggedIn: state.loggedIn,
+        cookies: ownProps.cookies,
+        spotifyToken: state.spotifyToken
+    };
+};
 
 class App extends React.Component {
   render(){
       return (
-        <HashRouter>
-          <Route path="/music-login">
-            <MusicLogin cookies={this.props.cookies}/>
-          </Route>
-          <Route exact path="/">
-            <Login cookies={this.props.cookies}/>
-          </Route>
-          <Route path="/signup">
-            <SignUp cookies={this.props.cookies}/>
-          </Route>
-          <Route path="/home">
-            <Home cookies={this.props.cookies}/>
-          </Route>
-          <Route path="/forgot-password">
-            <Password cookies={this.props.cookies}/>
-          </Route>
+        <SpotifyApiContext.Provider value={this.props.spotifyToken}>
+          <HashRouter>
+            <Route path="/music-login">
+              <MusicLogin cookies={this.props.cookies}/>
+            </Route>
+            <Route exact path="/">
+              <Login cookies={this.props.cookies}/>
+            </Route>
+            <Route path="/signup">
+              <SignUp cookies={this.props.cookies}/>
+            </Route>
+            <Route path="/home">
+              <Home cookies={this.props.cookies}/>
+            </Route>
+            <Route path="/forgot-password">
+              <Password cookies={this.props.cookies}/>
+            </Route>
         </HashRouter>
+        </SpotifyApiContext.Provider>
     )
   }
 }
 
-export default withCookies(App);
+export default withCookies(connect(mapStateToProps)(App));
--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {getSpotifyToken} from "../../redux/actions/authActions";
 import { useHistory } from "react-router-dom";
 const queryString = require('query-string');
 import { connect, useDispatch } from "react-redux";
 import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import { SpotifyApiContext, Artist } from 'react-spotify-api';
 
 const mapStateToProps = (state, ownProps) => {
     return {
@@ -17,6 +20,8 @@ const Home = (props) => {
     let history = useHistory();
     const dispatch = useDispatch();
     
+    const [searchValue, setSearchValue] = useState('');
+
     useEffect(() => {
         let parsed = queryString.parse(history.location.search);
         console.log(parsed.token);
@@ -25,10 +30,19 @@ const Home = (props) => {
             dispatch((getSpotifyToken(parsed.token)))
         }
     }, []);
+
+
     
     return (
         <div>
-            <TextField id="standard-basic" label="Standard" />
+            <Grid container justify="center" alignitems="center" spacing={3}>
+                <Grid item xs={3}>
+                    <TextField id="standard-basic" label="Standard" onChange={(e) => {setSearchValue(e.target.value)}}/>
+                    <Button variant="contained" color="primary">
+                        Search
+                    </Button>
+                </Grid>
+            </Grid>
         </div>        
     )
 }
